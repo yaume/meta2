c69a01cabf50a6e2e4d06b1b39a32edbcb0299df
META <meta@Mac-Pro.lan>
2018-10-13 16:58:58 +0200

    fifth commit

--- html/com_content/category/events.php
+++ html/com_content/category/events.php
@@ -71,7 +71,7 @@ $afterDisplayContent = trim(implode("\n", $results));
 
 	<?php $leadingcount = 0; ?>
 	<?php if (!empty($this->lead_items)) : ?>
-		<div class="items-leading clearfix">
+		<div class="items-leading ">
 			<?php foreach ($this->lead_items as &$item) : ?>
 				<div class="leading-<?php echo $leadingcount; ?><?php echo $item->state == 0 ? ' system-unpublished' : null; ?>"
 					itemprop="blogPost" itemscope itemtype="https://schema.org/BlogPosting">
@@ -95,19 +95,18 @@ $afterDisplayContent = trim(implode("\n", $results));
 			<?php $rowcount = ((int) $key % (int) $this->columns) + 1; ?>
 			<?php if ($rowcount === 1) : ?>
 				<?php $row = $counter / $this->columns; ?>
-				<div class="items-row cols-<?php echo (int) $this->columns; ?> <?php echo 'row-' . $row; ?> row clearfix">
+				<div class="items-row">
 			<?php endif; ?>
-			<div class="col-12 col-lg-<?php echo round(12 / $this->columns); ?>">
-				<div class="item column-<?php echo $rowcount; ?><?php echo $item->state == 0 ? ' system-unpublished' : null; ?>"
-					itemprop="itemListElement" itemscope itemtype="http://schema.org/Event">
-					<?php
-					$this->item = &$item;
-					echo $this->loadTemplate('item');
-					?>
-				</div>
-				<!-- end item -->
-				<?php $counter++; ?>
-			</div><!-- end span -->
+			<div class="item column-<?php echo $rowcount; ?><?php echo $item->state == 0 ? ' system-unpublished' : null; ?>"
+				itemprop="itemListElement" itemscope itemtype="http://schema.org/Event">
+				<?php
+				$this->item = &$item;
+				echo $this->loadTemplate('item');
+				?>
+			</div>
+			<!-- end item -->
+			<?php $counter++; ?>
+		<!-- end span -->
 			<?php if (($rowcount == $this->columns) or ($counter == $introcount)) : ?>
 				</div><!-- end row -->
 			<?php endif; ?>
