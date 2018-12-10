1a915ddc51cfaf96f7dbf9e9f8305ffe2888656a
META <meta@Mac-Pro.lan>
2018-12-10 18:38:38 +0100

    contact form

--- html/modules.php
+++ html/modules.php
@@ -26,6 +26,17 @@ defined('_JEXEC') or die;
 /*
  * Module chrome for rendering the module in a submenu
  */
+function modChrome_contact($module, &$params, &$attribs)
+{
+	$headerLevel = isset($attribs['headerLevel']) ? (int) $attribs['headerLevel'] : 3;
+	if (!empty ($module->content)) : ?>
+		<div class="moduletable<?php echo htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8'); ?>">
+		<?php if ($module->showtitle) : ?>
+			<h<?php echo $headerLevel; ?>><?php echo $module->title; ?></h<?php echo $headerLevel; ?>>
+		<?php endif; ?>
+		<?php echo $module->content; ?></div>
+	<?php endif;
+}
 function modChrome_no($module, &$params, &$attribs)
 {
   if ($module->content)
